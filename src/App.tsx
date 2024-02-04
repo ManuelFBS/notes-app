b7a53661d394922212deb5d72715ffdc9d08f9d9
ManuelFBS <manuelf.borrego@gmail.com>
2024-02-04 00:19:08 -0400

    Modificación de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <form className='note-form'>
+        <input placeholder='Title' required />
+        <textarea placeholder='Content' rows={10} required />
+
+        <button type='submit'>Agregar Nota</button>
+      </form>
+      <div className='notes-grid'>
+        <div className='note-item'>
+          <div className='notes-header'>
+            <button>x</button>
+          </div>
+          <h2>Título de la Nota</h2>
+          <p>Note content...</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
