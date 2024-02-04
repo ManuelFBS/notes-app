53610852b76ec944369a9a25298348c9de3072b5
ManuelFBS <manuelf.borrego@gmail.com>
2024-02-04 12:05:00 -0400

    Actualización de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,39 @@ import './App.css';
 import React, { useState } from 'react';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: 'Test #1',
+      content: 'This is a test... #1'
+    },
+    {
+      id: 2,
+      title: 'Test #2',
+      content: 'This is a test... #2'
+    },
+    {
+      id: 3,
+      title: 'Test #3',
+      content: 'This is a test... #3'
+    },
+    {
+      id: 4,
+      title: 'Test #4',
+      content: 'This is a test... #4'
+    },
+    {
+      id: 5,
+      title: 'Test #5',
+      content: 'This is a test... #5'
+    },
+    {
+      id: 6,
+      title: 'Test #6',
+      content: 'This is a test... #6'
+    }
+  ]);
+
   return (
     <div className='app-container'>
       <form className='note-form'>
@@ -11,13 +44,15 @@ const App = () => {
         <button type='submit'>Add Note</button>
       </form>
       <div className='note-grid'>
-        <div className='note-item'>
-          <div className='notes-header'>
-            <button>x</button>
+        {notes.map((note) => (
+          <div className='note-item'>
+            <div className='notes-header'>
+              <button>x</button>
+            </div>
+            <h2>Note Title</h2>
+            <p>Note Content</p>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
-        </div>
+        ))}
       </div>
     </div>
   );
