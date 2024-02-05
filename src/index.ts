f0af5b49786a6b94cb9e45d0eae53be92ef953bf
ManuelFBS <manuelf.borrego@gmail.com>
2024-02-05 18:59:48 -0400

    Actualización de index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,40 @@ app.get('/api/notes', async (req, res) => {
 
 app.post('/api/notes', async (req, res) => {
   const { title, content } = req.body;
-  const notes = await prisma.note.create({
-    data: { title, content }
-  });
 
-  res.json(notes);
+  if (!title || !content) {
+    return res.status(400).send('title and content fields required');
+  }
+
+  try {
+    const note = await prisma.note.create({
+      data: { title, content }
+    });
+
+    res.json(note);
+  } catch (error) {
+    res.status(500).send('Something went wrong...!');
+  }
+});
+
+app.put('/api/notes/:id', async (req, res) => {
+  const { title, content } = req.body;
+  const id = parseInt(req.params.id);
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send('ID must be a valid number');
+  }
+
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id },
+      data: { title, content }
+    });
+
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(500).send('Something went wrong...');
+  }
 });
 
 app.listen(7000, () => {
