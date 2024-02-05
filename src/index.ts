a04656ea0b7a9adc38f41bb66801fc10ba2786e0
ManuelFBS <manuelf.borrego@gmail.com>
2024-02-05 02:34:25 -0400

    Actualización de index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/api/notes', async (req, res) => {
+  res.json({ message: 'Success !!!' });
+});
+
 app.listen(7000, () => {
   console.log('Server is running on localhost:7000');
 });
