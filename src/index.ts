7e6e24f1fb352a83dd70dc1aa9a20001e4b70f1a
ManuelFBS <manuelf.borrego@gmail.com>
2024-02-05 12:35:31 -0400

    Actualización de App.ts y .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/api/notes', async (req, res) => {
-  res.json({ message: 'Success !!!' });
+  const notes = await prisma.note.findMany();
+
+  res.json(notes);
+});
+
+app.post('/api/notes', async (req, res) => {
+  const { title, content } = req.body;
+  const notes = await prisma.note.create({
+    data: { title, content }
+  });
+
+  res.json(notes);
 });
 
 app.listen(7000, () => {
